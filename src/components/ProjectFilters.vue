62f570531917fb8aae47804aef32a8864715a73e
Guilbert Pierre <pierre.guilbert@epfl.ch>
2024-04-24 12:11:42 +0200

    feat(frontend): add default search filter

--- src/components/ProjectFilters.vue
+++ src/components/ProjectFilters.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { mdiMapLegend } from '@mdi/js'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useDisplay } from 'vuetify'
+import { useFiltersStore } from '@/stores/filters';
+import { storeToRefs } from 'pinia';
 
 const { t, locale } = useI18n({ useScope: 'global' })
 const { mobile } = useDisplay()
 
-const filters = [
+const filtersSelect = [
   { key: 'main_concrete_type', values: ['PC', 'CIP'] },
   { key: 'receiver_location_country', values: ['DE', 'SE', 'FR', 'IT'] },
   { key: 'source_use', values: ['Bridge', 'Building', 'Tunnel', 'Other'] },
@@ -16,33 +18,74 @@ const filters = [
   { key: 'receiver_element_type', values: ['Beam', 'Column', 'Slab', 'Wall'] }
 ]
 
+const filtersRange = [
+  { key: 'distance_km', values: [0, 100] },
+  { key: 'precs_start_date_year', values: [0, 100] },
+  { key: 'component_age_at_start_date', values: [0, 100] },
+  { key: 'source_nb_floor', values: [0, 100] },
+  { key: 'receiver_nb_floor', values: [0, 100] },
+]
+
 const main_concrete_type = ref<any>({
-  PC: "Precast",
-  CIP: "Cast in place",
+  PC: 'Precast',
+  CIP: 'Cast in place'
 })
 
-let filtersSelected = ref<any>({})
+const store = useFiltersStore()
+const { filters } = storeToRefs(store)
+const { setFilters } = store
+
+// const filters = computed({
+//   get: () => store.getFilters,
+//   set: (value) => store.setFilters(value)
+// })
+
+const props = withDefaults(
+  defineProps<{
+    isVisible?: boolean
+  }>(),
+  {
+    isVisible: false
+  }
+)
 </script>
 
 <template>
   <v-list-item :prepend-icon="mdiMapLegend">
-    <v-list-item-title>
+    <v-list-item-title v-show="props.isVisible">
       <span :class="mobile ? 'text-subtitle-1' : 'text-h6'">{{ $t('filters') }}</span>
     </v-list-item-title>
   </v-list-item>
-  <v-list-item v-for="(filter, $key) in filters" :key="$key">
+  <v-list-item v-show="props.isVisible">
+    <v-row>
+      <v-col cols="6">
+        {{ $t('search') }}
+      </v-col>
+      <v-col cols="6">
+        <v-text-field v-model:model-value="filters.name" @update:model-value="() => setFilters(filters)"
+          :clearable="true" />
+      </v-col>
+    </v-row>
+  </v-list-item>
+  <v-list-item v-for="(filter, $key) in filtersSelect" :key="$key" v-show="props.isVisible">
     <v-row>
       <v-col cols="6">
         {{ $t(`project_${filter.key}`) }}
       </v-col>
       <v-col cols="6">
-        <v-select
-          v-model:focused="filtersSelected[filter.key]"
-          clearable
-          multiple
-          chips
-          :items="filter.values"
-        />
+        <v-select v-model="filters[filter.key]" @update:model-value="() => setFilters(filters)" :clearable="true"
+          multiple chips :items="filter.values" />
+      </v-col>
+    </v-row>
+  </v-list-item>
+  <v-list-item v-for="(filter, $key) in filtersRange" :key="$key" v-show="props.isVisible">
+    <v-row>
+      <v-col cols="6">
+        {{ $t(`project_${filter.key}`) }}
+      </v-col>
+      <v-col cols="6">
+        <v-range-slider v-model="filters[filter.key]" @update:model-value="() => setFilters(filters)" clearable multiple
+          chips :items="filter.values" />
       </v-col>
     </v-row>
   </v-list-item>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+// import projects from '@/assets/data.json'
+import ProjectCard from '@/components/ProjectCard.vue'
+import ProjectDialog from '@/components/ProjectDialog.vue'
+import ProjectFilters from '@/components/ProjectFilters.vue'
+import { useProjectsStore } from '@/stores/projects'
+import { mdiChevronLeft, mdiChevronRight } from '@mdi/js'
+import { storeToRefs } from 'pinia'
 import { computed, ref } from 'vue'
-import { useRouter } from 'vue-router';
-import { mdiChevronRight, mdiChevronLeft } from '@mdi/js'
-import { useDisplay } from 'vuetify'
 import { useI18n } from 'vue-i18n'
-import projects from "@/assets/data.json";
-import ProjectFilters from '@/components/ProjectFilters.vue';
-import ProjectDialog from '@/components/ProjectDialog.vue';
+import { useRouter } from 'vue-router'
+import { useDisplay } from 'vuetify'
 
 const { t, locale } = useI18n({ useScope: 'global' })
 const { mobile } = useDisplay()
@@ -20,16 +23,17 @@ const listMode = computed({
   }
 })
 
-const data = ref<any>(false)
-const drawerRail = ref(false)
-data.value = projects
+const projects = storeToRefs(useProjectsStore()).projects
 
-const isProjectDialogOpen = ref(false);
-const projectSelected = ref<any>(false);
+const data = projects;
+const drawerRail = ref(false)
+
+const isProjectDialogOpen = ref(false)
+const projectSelected = ref<any>(false)
 
 const selectProject = (project: any) => {
-  projectSelected.value = project;
-  isProjectDialogOpen.value = true;
+  projectSelected.value = project
+  isProjectDialogOpen.value = true
 }
 
 const onRowClicked = (ref: any, row: any) => {
@@ -37,16 +41,20 @@ const onRowClicked = (ref: any, row: any) => {
 }
 
 const headers = [
-  { title: t("project__id"), value: "_id", sortable: false },
+  { title: t('project__id'), value: '_id', sortable: false },
   // { title: t("project_description"), value: "description", sortable: false},
-  { title: t("project_name"), value: "name", sortable: false },
-  { title: t("project_main_concrete_type"), value: "main_concrete_type", sortable: false },
+  { title: t('project_name'), value: 'name', sortable: false },
+  { title: t('project_main_concrete_type'), value: 'main_concrete_type', sortable: false },
   // { title: t("project_main_concrete_type_uncertainty"), value: "main_concrete_type_uncertainty", sortable: false},
   // { title: t("project_receiver_location_coordinates"), value: "receiver_location_coordinates", sortable: false},
-  { title: t("project_receiver_location_country"), value: "receiver_location_country", sortable: false },
-  { title: t("project_receiver_location_city"), value: "receiver_location_city", sortable: false },
-  { title: t("project_distance_km"), value: "distance_km", sortable: false },
-  { title: t("project_precs_start_date_year"), value: "precs_start_date_year", sortable: false },
+  {
+    title: t('project_receiver_location_country'),
+    value: 'receiver_location_country',
+    sortable: false
+  },
+  { title: t('project_receiver_location_city'), value: 'receiver_location_city', sortable: false },
+  { title: t('project_distance_km'), value: 'distance_km', sortable: false },
+  { title: t('project_precs_start_date_year'), value: 'precs_start_date_year', sortable: false }
   // { title: t("project_component_age_at_start_date"), value: "component_age_at_start_date", sortable: false},
   // { title: t("project_quantity_reclaimed_ton"), value: "quantity_reclaimed_ton", sortable: false},
   // { title: t("project_source_nb_floor"), value: "source_nb_floor", sortable: false},
@@ -68,45 +76,31 @@ const headers = [
 </script>
 
 <template>
-  <v-navigation-drawer :rail="drawerRail" permanent :width="mobile ? 300 : 450" @click="drawerRail = false">
+  <v-navigation-drawer
+    :rail="drawerRail"
+    permanent
+    :width="mobile ? 300 : 450"
+    @click="drawerRail = false"
+  >
     <v-list density="compact" nav>
       <v-list-item :prepend-icon="drawerRail ? mdiChevronRight : undefined">
         <template #append>
           <v-btn :icon="mdiChevronLeft" variant="flat" @click.stop="drawerRail = true" />
         </template>
       </v-list-item>
-
-      <project-filters />
+      <project-filters :is-visible="!drawerRail" />
     </v-list>
   </v-navigation-drawer>
-  <v-container class="fill-height pa-0" fluid v-if="listMode === 'list'">
+  <v-container v-if="listMode === 'list'" class="fill-height pa-0 align-baseline" fluid>
     <v-data-table :items="data" :headers="headers" @click:row="onRowClicked"></v-data-table>
   </v-container>
-  <v-container class="fill-height pa-0 grid-list" fluid v-if="listMode === 'grid'">
-    <!-- <v-row class="fill-height">
-      <v-col cols="12" class="py-0"> -->
-        <v-card :key="$key" v-for="(item, $key) in data" class="mx-auto" min-width="400" max-width="400" max-height="400" :min-height="400" outlined tile
-          @click="() => selectProject(item)">
-          <v-img class="align-end text-white" height="200" src="beton.png" cover>
-            <v-card-title>{{ item.name }}</v-card-title>
-          </v-img>
-
-          <v-card-subtitle class="pt-4">
-            {{ item._id }}
-          </v-card-subtitle>
-
-          <v-card-text>
-            {{ item.description }}
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="orange" text="Share"></v-btn>
-
-            <v-btn color="orange" text="Explore"></v-btn>
-          </v-card-actions>
-        </v-card>
-      <!-- </v-col>
-    </v-row> -->
+  <v-container v-if="listMode === 'grid'" class="fill-height pa-0 grid-list" fluid>
+    <project-card
+      v-for="(item, $key) in data"
+      :key="$key"
+      :item="item"
+      @click="() => selectProject(item)"
+    />
   </v-container>
   <project-dialog v-model="isProjectDialogOpen" :project="projectSelected" />
 </template>
