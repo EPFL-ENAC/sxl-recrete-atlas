9a7224d93d859e7d0884f636ebe70f300ca4af4e
Guilbert Pierre <pierre.guilbert@epfl.ch>
2024-05-17 08:32:40 +0200

    feat: transform main-concrete in string[] instead of string

--- scripts/convert.js
+++ scripts/convert.js
@@ -6,9 +6,10 @@ csv({ checkType: true })
   .then((jsonObj) => {
     const path = './src/assets/data/keys.json';
     writeFileSync(path, JSON.stringify(jsonObj, null, 2));
-    console.log(`data.csv converted successfully to JSON in ${path}`)
+    console.log(`keys.csv converted successfully to JSON in ${path}`)
   });
 
+const arraySplitter = (item) => item.split(",").map((type) => type.trim());
 csv({ checkType: true, ignoreEmpty: true, trim: true,
   colParser:{
     "receiver_coordinates":function(item){
@@ -16,6 +17,8 @@ csv({ checkType: true, ignoreEmpty: true, trim: true,
         .map((coordinate) => parseFloat(coordinate.trim()))
         .reverse();
     },
+    "main_concrete_type": arraySplitter,
+    "main_concrete_type_uncertainty": arraySplitter,
     "images":function(item){
         return item.split(",").map((image) => `/images/${image.trim()}`);
     },
