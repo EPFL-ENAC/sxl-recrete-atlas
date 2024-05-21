a9a5b26c25a3fc8ad7713294ecd697e1d9ca1638
Guilbert Pierre <pierre.guilbert@epfl.ch>
2024-05-21 11:26:02 +0200

    chore: add eslint/recommended
    Former-commit-id: b014001664d1d4c73143a7db7a0c474cd65293ac

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    // '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
