7c47134a38bc374624276a4d5292b7b8d9e392dc
Guilbert Pierre <pierre.guilbert@epfl.ch>
2024-05-24 15:14:44 +0200

    fix(frontend): new component for tooltip
    Former-commit-id: 0f5c2f24a642c8cbbd91fe3b492905fa1abf8e3d

---
+++ src/components/ReferenceList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+              <li v-for="(refe, key) in item?.reference" :key="key">
+                
+                <span  v-if="referencesMap[refe].url" >
+
+                  [{{ refe }}]: <a target="_blank" :href="referencesMap[refe].url">
+                    
+                   {{ referencesMap[refe].text }}
+                  
+                  </a>
+                </span>
+                <span v-else>
+                  [{{ refe }}]: {{ referencesMap[refe].text }}
+                </span>
+              </li>
+            </ul>
+</template>
+
+<script setup lang="ts">
+import { useReferencesStore } from '@/stores/references'
+import { storeToRefs } from 'pinia';
+import { defineProps } from 'vue'
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+
+const referencesMap = storeToRefs(useReferencesStore()).referencesMap
+
+
+
+</script>
+
+<style scoped lang="scss">
+ul {
+  list-style: lower-latin;
+  li {
+    margin-bottom: 0.5rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
