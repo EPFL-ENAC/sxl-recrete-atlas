6bc1d62fc42f4c39f2075331d16d32d52ff74c7f
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-05-22 21:44:22 +0200

    fix: update image processing functions to use toWebp1920 for improved image quality
    Former-commit-id: 706e5a0f8cd9ff537c9f3e5957d283efdc79e50d

--- src/utils/image.js
+++ src/utils/image.js
@@ -9,3 +9,11 @@ export function toWebp512(imagePath) {
   if (imagePath.endsWith('-512.webp')) return imagePath
   return imagePath.replace(/\.(jpg|jpeg|png|webp)$/i, '-512.webp')
 }
+
+
+export function toWebp1920(imagePath) {
+  if (!imagePath) return ''
+  imagePath = imagePath.trim()
+  if (imagePath.endsWith('-1920.webp')) return imagePath
+  return imagePath.replace(/\.(jpg|jpeg|png|webp)$/i, '-1920.webp')
+}
